588c1f971ab7a50d26a063fb3bb17a399e6811dc
Nicholas <ngidaro@hotmail.com>
2020-12-02 19:53:04 -0500

    Removed log message

--- routes/api/food.js
+++ routes/api/food.js
@@ -10,7 +10,7 @@ var Diary = require('../../models/diary');
 router.post('/', /* isAuthenticated */ async function ( req, res, next ){
 
     // const api_url = (`https://api.nal.usda.gov/fdc/v1/food/559594?api_key=zsGhFEwjHoRwhrNxJqBaeUDuL237of0dpKxqQL6k`);
-    const api_url = (`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${API_KEY}&query=${req.body.search}`);
+    const api_url = (`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${API_KEY}&query=${req.body.search}&dataType=Branded`);
     
     const response = await fetch(api_url);
     const json = await response.json();
@@ -23,8 +23,6 @@ router.post('/fooddata', /* isAuthenticated */ async function ( req, res, next )
 
     const api_url = (`https://api.nal.usda.gov/fdc/v1/food/${req.body.fdcId}?api_key=${API_KEY}`);
     
-    console.log(req.body.fdcId);
-
     const response = await fetch(api_url);
     const json = await response.json();
     console.log(json);
@@ -39,9 +37,6 @@ router.post('/savediary', /* isAuthenticated */ async function ( req, res, next 
     const date = dateUTC(req.body.date);
 
     try {
-
-        console.log(req.body.user_id);
-        console.log(req.body);
 
         var newDiaryEntry = new Diary();
             newDiaryEntry.userid = req.body.user_id;
